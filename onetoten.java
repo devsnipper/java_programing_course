79c9c45fde9e06aa02653dcce32abdb9490dfba8
devsnipper <eyramafatchao@gmail.com>
2025-03-04 07:07:50 +0000

    Add files via upload

---
+++ onetoten.java
@@ -0,0 +1,14 @@
+package chp4;
+
+public class onetoten {
+    public static void main(String[] args) {
+        for (int i=1;i<=10;i++){
+            if(i<=0){
+                continue;
+            }
+            System.out.println(i);
+
+        }
+    }
+
+}
